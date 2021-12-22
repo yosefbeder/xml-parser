88e3b7034253deffbcbbeddefbe1d52e88c53bb1
yosefbeder <dryosefbeder@gmail.com>
2021-12-22 06:52:43 +0200

    feat: add identifier parser

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hello world');
+const {
+	isAlphabetical,
+	isNumeric,
+	pred,
+	anyCharacter,
+	zeroOrMore,
+} = require('./utils');
+
+const identifier = input => {
+	const [nextInput1, result1] = pred(anyCharacter, result =>
+		isAlphabetical(result),
+	)(input);
+
+	const [nextInput2, result2] = zeroOrMore(
+		pred(
+			anyCharacter,
+			result => isAlphabetical(result) || isNumeric(result) || result === '-',
+		),
+	)(nextInput1);
+
+	return [nextInput2, [result1, ...result2].join('')];
+};
+
+module.exports = {
+	identifier,
+};
--- index.test.js
+++ index.test.js
@@ -1,3 +1,15 @@
-test('Everything works', () => {
-	expect(1).toBe(1);
+const { identifier } = require('./index.js');
+
+describe('identifier', () => {
+	it('should match alphabetical, numeric, and - and return the rest of the string', () => {
+		expect(identifier('yosef123')).toEqual(['', 'yosef123']);
+		expect(identifier('yosef-beder')).toEqual(['', 'yosef-beder']);
+		expect(identifier('yosef')).toEqual(['', 'yosef']);
+		expect(identifier('yosef beder')).toEqual([' beder', 'yosef']);
+	});
+
+	it('should throw an error if the first character of an identifier is a not alphatical', () => {
+		expect(() => identifier('1jj')).toThrowError('1jj');
+		expect(() => identifier('!jj')).toThrowError('!jj');
+	});
 });
